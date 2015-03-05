02313046f62a46dc3d716ee927128c1c2a5973d1
Ben Garren <bngarren@gmail.com>
2015-03-05 10:18:43 -0500

    Changed welcome message

--- GitTest/src/com/application/Main.java
+++ GitTest/src/com/application/Main.java
@@ -5,7 +5,8 @@ import com.user.User;
 public class Main {
 
 	public static void main(String[] arg){
-		System.out.println("This is GitTest!");
+		System.out.println("Welcome to GitTest!");
+		System.out.println("This is our user:");
 
 		User user = new User("bngarren");
 		user.display();
